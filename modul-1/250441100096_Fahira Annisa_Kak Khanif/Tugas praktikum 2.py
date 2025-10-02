9d09e977051baa1d609af65964c3d3635e6ca002
Fahira Annisa <fahirann6@gmail.com>
2025-10-02 14:31:04 +0700

    250441100096_Fahira Annisa_MODUL 1_Kak Khanif

---
+++ modul-1/250441100096_Fahira Annisa_Kak Khanif/Tugas praktikum 2.py
@@ -0,0 +1,11 @@
+# B. Menghitung total biaya 
+harga_buku = 45000
+jumlah_buku = 3
+total_biaya_buku = jumlah_buku * harga_buku
+
+harga_pulpen = 7500
+jumlah_pulpen = 2
+total_biaya_pulpen = jumlah_pulpen * harga_pulpen
+total_biaya = total_biaya_buku + total_biaya_pulpen
+
+print(total_biaya)
---
+++ modul-1/250441100096_Fahira Annisa_Kak Khanif/Tugas praktikum 3.py
@@ -0,0 +1,7 @@
+# C. Jumlah uang kembaliannya
+uang_yang_dibayar = 200000
+uang_kembalian = uang_yang_dibayar - total_biaya # Rp.200.000 - total_biaya
+
+
+print("===hasil===") 
+print(uang_kembalian)
