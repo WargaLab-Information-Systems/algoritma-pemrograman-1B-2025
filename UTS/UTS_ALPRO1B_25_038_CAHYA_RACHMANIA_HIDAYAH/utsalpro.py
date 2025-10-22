df6c6da268b7cd0092ba296a00f16a123cb2e703
zainulakbar590 <zainulakbar590@gmail.com>
2025-10-22 12:23:27 -0700

    250441100038_Cahya_Rachmani_Hidayah_uts

---
+++ UTS/UTS_ALPRO1B_25_038_CAHYA_RACHMANIA_HIDAYAH/utsalpro.py
@@ -0,0 +1,14 @@
+#harga sewa
+rental_motor =int(input("harga sewa:"))
+motor_metic = 50000
+motor_trail = 100000
+motor_sport = 75000
+
+waktu_lebih_harga_sewa = 15000 >3
+diskon = 0.10 > 150000
+diskon_kupon = 0.5
+
+
+
+
+
